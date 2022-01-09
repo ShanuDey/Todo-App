340a6da9218c793e57aa9aed68404155c7227871
Shanu Dey <sd4shanudey@gmail.com>
2022-01-09 11:31:58 +0530

    added todos context
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Footer from './src/components/Footer';
 import TodoList from './src/components/TodoList';
 import Header from './src/components/Header';
 import Constants from 'expo-constants';
+import { TodosContext } from './src/contexts/TodosContext';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      name: 'Todo 1',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
+    },
+    {
+      id: 2,
+      name: 'Todo 2',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
+    },
+    {
+      id: 3,
+      name: 'Todo 3',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
+    },
+    {
+      id: 4,
+      name: 'Todo 4',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
+    },
+    {
+      id: 5,
+      name: 'Todo 5',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
+    },
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
-      <TodoList />
-      <Footer />
+      <TodosContext.Provider value={{ todos, setTodos }}>
+        <TodoList />
+        <Footer />
+      </TodosContext.Provider>
     </View>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,40 +1,11 @@
-import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { TodosContext } from '../contexts/TodosContext';
 import Todo from './Todo/Todo';
 
 export default function TodoList() {
-  const todos = [
-    {
-      id: 1,
-      name: 'Todo 1',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
-    },
-    {
-      id: 2,
-      name: 'Todo 2',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
-    },
-    {
-      id: 3,
-      name: 'Todo 3',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
-    },
-    {
-      id: 4,
-      name: 'Todo 4',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
-    },
-    {
-      id: 5,
-      name: 'Todo 5',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?',
-    },
-  ];
+  const { todos, setTodos } = useContext(TodosContext);
+
   return (
     <View style={styles.container}>
       <ScrollView>
