d21bee74f60230cf0daeaefabb6a56c0c85fd576
Shanu Dey <sd4shanudey@gmail.com>
2022-01-09 12:19:31 +0530

    added new todo at the top
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { View, StyleSheet, TextInput, Button } from 'react-native';
+import { TodosContext } from '../contexts/TodosContext';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Footer() {
+  const { todos, setTodos } = useContext(TodosContext);
+  const [newTodo, setNewTodo] = useState('');
+
+  function addTodoHandler() {
+    let currentName =
+      newTodo.length <= 10 ? newTodo : `${newTodo.substring(0, 10)}...`;
+    setTodos([
+      {
+        key: uuidv4(),
+        name: currentName,
+        description: newTodo,
+      },
+      ...todos,
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Type your Todo here' style={styles.input} />
-      <Button title='Add' style={styles.button} />
+      <TextInput
+        placeholder='Type your Todo here'
+        style={styles.input}
+        onChangeText={(text) => setNewTodo(text)}
+      />
+      <Button title='Add' style={styles.button} onPress={addTodoHandler} />
     </View>
   );
 }
