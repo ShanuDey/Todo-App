a13b798569b3059936462349e34d1d759206b005
Shanu Dey <sd4shanudey@gmail.com>
2022-01-09 10:32:03 +0530

    Footer design ready
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
 
 export default function Footer() {
   return (
     <View style={styles.container}>
-      <Text>Footer</Text>
+      <TextInput placeholder='Type your Todo here' style={styles.input} />
+      <Button title='Add' style={styles.button} />
     </View>
   );
 }
@@ -12,8 +13,19 @@ export default function Footer() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    margin: 10,
+  },
+  input: {
+    flex: 2,
+    margin: 5,
+    borderWidth: 1,
+    padding: 2,
+    borderColor: '#1b6110',
+  },
+  button: {
+    flex: 1,
   },
 });
